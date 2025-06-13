7e0e9bf17c21b344f15ab9b920a82dc678b8ac58
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-13 20:42:18 +0600

    find user name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className='flex items-center justify-center h-screen'>
       <UserButton />
-      
+      {user?.firstName} {user?.lastName} 
       This is Khandker Mohyet's work station
       
     </div>
